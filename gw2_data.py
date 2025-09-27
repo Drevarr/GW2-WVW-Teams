dec6532ebf5b9b752455755efc5a42822e146034
Drevarr <drevarr@gmail.com>
2025-09-27 14:47:52 -0400

    update NA_wvw_teams

--- gw2_data.py
+++ gw2_data.py
@@ -1,16 +1,15 @@
 wvw_matches = ['1-1', '1-2', '1-3', '1-4']
 
 NA_wvw_teams = {
-    "11001": "Moogooloo",
-    "11002": "Rall's Rest",
-    "11003": "Domain of Torment",
-    "11004": "Yohlon Haven",
-    "11005": "Tomb of Drascir",
-    "11006": "Hall of Judgement",
-    "11007": "Throne of Balthazar",
-    "11008": "Dwayna's Temple",
-    "11009": "Abaddon's Prison",
-    "11010": "Ruined Cathedral of Blood",
-    "11011": "Lutgardis Conservatory",
-    "11012": "Mosswood"
-}
+    '11009': "Abaddon's Prison",
+    '11010': 'Cathedral of Blood',
+    '11003': 'Domain of Torment',
+    '11008': "Dwayna's Temple",
+    '11006': 'Hall of Judgment',
+    '11011': 'Lutgardis Conservatory',
+    '11001': 'Moogooloo',
+    '11012': 'Mosswood',
+    '11002': "Rall's Rest",
+    '11007': 'Throne of Balthazar',
+    '11005': 'Tomb of Drascir',
+    '11004': 'Yohlon Haven'}
